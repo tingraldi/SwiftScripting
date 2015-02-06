3c504cc26752d178e0ea9fe66eb8f7f2a898ae8e
Tony Ingraldi <a.m.ingraldi@majestysoftware.com>
2015-02-06 18:45:47 -0500

    PEP 8 (most of it)

--- sbhc.py
+++ sbhc.py
@@ -1,4 +1,4 @@
-__author__ = 'tingraldi'
+#!/usr/bin/env python2.7
 
 import sys
 
@@ -17,19 +17,18 @@ keywords = ['class', 'deinit', 'enum', 'extension', 'func', 'import', 'init', 'i
             'optional', 'override', 'postfix', 'precedence', 'prefix', 'Protocol', 'required', 'right', 'set', 'Type',
             'unowned', 'weak', 'willSet']
 
-type_dict = {
-             'BOOL': 'Bool',
+type_dict = {'BOOL': 'Bool',
              'double': 'Double',
              'NSInteger': 'Int',
              'NSString': 'String',
              'id': 'AnyObject',
              'NSArray': '[AnyObject]',
-             'NSDictionary': '[NSObject : AnyObject]'
-            }
+             'NSDictionary': '[NSObject : AnyObject]'}
 
 
 def safe_name(name):
     return '`{}`'.format(name) if name in keywords else name
+
 
 def type_for_type(objc_type):
     obj_type_string = objc_type.spelling.split(" ")[0]
@@ -37,6 +36,7 @@ def type_for_type(objc_type):
     if repr(objc_type.kind).startswith('TypeKind.OBJC'):
         mapped_type += '!'
     return mapped_type
+
 
 def name_from_path(path):
     last_part = path.split("/")[-1]
@@ -61,7 +61,7 @@ class SBHeaderProcessor(object):
     def emit_enums(self):
         enum_file = open('{}Enums.h'.format(self.app_name), 'w')
         typedefs = [line for line in self.lines if line.startswith('typedef')]
-        class_lines = [line for line in self.lines if line.startswith(('@class'))]
+        class_lines = [line for line in self.lines if line.startswith('@class')]
         start_line_index = self.lines.index(class_lines[-1]) + 1
         last_typedef_line_index = self.lines.index(typedefs[-1]) + 1
         for index in xrange(start_line_index, last_typedef_line_index):
@@ -78,23 +78,27 @@ class SBHeaderProcessor(object):
             if word == 'readonly':
                 get_set = 'get'
         swift_type = type_for_type(cursor.type)
-        self.emit_line('    optional var {}: {} {{ {} }}{}'.format(cursor.spelling, swift_type, get_set, self.line_comment(cursor)))
+        self.emit_line('    optional var {}: {} {{ {} }}{}'.format(cursor.spelling, swift_type, get_set,
+                                                                   self.line_comment(cursor)))
 
     def emit_function(self, cursor, accessors):
         if cursor.spelling not in accessors:
             func_name = cursor.spelling.split(':')[0]
-            parameters = ['{}: {}'.format(safe_name(child.spelling), type_for_type(child.type)) for child in cursor.get_children() if child.kind == CursorKind.PARM_DECL]
+            parameters = ['{}: {}'.format(safe_name(child.spelling), type_for_type(child.type))
+                          for child in cursor.get_children() if child.kind == CursorKind.PARM_DECL]
             return_type = [child.type for child in cursor.get_children() if child.kind != CursorKind.PARM_DECL]
             if return_type:
                 return_string = ' -> {}'.format(type_for_type(return_type[0]))
             else:
                 return_string = ''
-            self.emit_line('    optional func {}({}){}{}'.format(func_name, ", ".join(parameters), return_string, self.line_comment(cursor)))
+            self.emit_line('    optional func {}({}){}{}'.format(
+                func_name, ", ".join(parameters), return_string, self.line_comment(cursor)))
 
     def emit_protocol(self, cursor):
         self.emit_line('@objc protocol {} {{'.format(cursor.spelling))
         superclass = 'SBObject'
-        property_accessors = [child.spelling for child in cursor.get_children() if child.kind == CursorKind.OBJC_PROPERTY_DECL]
+        property_accessors = [child.spelling for child in cursor.get_children()
+                              if child.kind == CursorKind.OBJC_PROPERTY_DECL]
         property_setters = ['set{}{}:'.format(getter[0].capitalize(), getter[1:]) for getter in property_accessors]
         property_accessors += property_setters
         for child in cursor.get_children():
@@ -108,7 +112,8 @@ class SBHeaderProcessor(object):
         self.emit_line('extension {} : {} {{}}\n'.format(superclass, cursor.spelling))
 
     def walk(self, cursor):
-        local_children = [child for child in cursor.get_children() if child.location.file and child.location.file.name == self.file_path]
+        local_children = [child for child in cursor.get_children()
+                          if child.location.file and child.location.file.name == self.file_path]
         for child in local_children:
             if child.kind in (CursorKind.OBJC_INTERFACE_DECL, CursorKind.OBJC_CATEGORY_DECL):
                 self.emit_protocol(child)
