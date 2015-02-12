0b66bba238a60e2130bf8cd9c16d1e4b2e2364a7
Tony Ingraldi <a.m.ingraldi@majestysoftware.com>
2015-02-11 22:35:24 -0500

    Add sample Acorn script

---
+++ Samples/Acorn/Scale and Export as PNG.swift
@@ -0,0 +1,61 @@
+#!/usr/bin/xcrun swift -F /Library/Frameworks
+
+//  Copyright (c) 2015 Majesty Software.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+// Acorn sample script.
+// Exports the frontmost document open in Acorn and exports it as PNG
+// in three different sizes.
+//
+// @3x - same size as source image
+// @2x - 2/3 the size of the source image
+// 1x  - 1/3 the size of the source image
+//
+// Images are exported to the Images folder on the user's Desktop. The Images
+// folder is created if it doesn't already exist.
+//
+
+import Foundation
+import ScriptingUtilities
+import FinderScripting
+import AcornScripting
+
+let finder = Application(name: "Finder") as FinderApplication
+let acorn = Application(name: "Acorn") as AcornApplication
+
+let imageFolderURL = NSURL(fileURLWithPath:"~/Desktop/Images".stringByExpandingTildeInPath)!
+NSFileManager.defaultManager().createDirectoryAtURL(imageFolderURL, withIntermediateDirectories: true, attributes: nil, error: nil)
+
+let document = acorn.documents!().objectAtLocation(0) as AcornDocument
+let name = document.name!.stringByDeletingPathExtension
+
+let folder = finder.folders!().objectAtLocation(imageFolderURL) as FinderFolder
+folder.openUsing!(nil, withProperties: nil)
+finder.activate()
+
+for (scale, suffix) in [1.0 : "@3x", 0.66 : "@2x", 0.33 : ""] {
+    let fileName = "\(name)\(suffix).png"
+    let fileURL: NSURL = imageFolderURL.URLByAppendingPathComponent(fileName)
+    let scaledWidth = Int(scale * document.width!)
+    let scaledHeight = Int(scale * document.height!)
+    document.webExportIn!(fileURL, `as`: AcornSaveableFileFormatPNG, quality: 100, width: scaledWidth, height: scaledHeight)
+}
+
+NotifyUserWithMessage("Export complete.", title: "Acorn")
